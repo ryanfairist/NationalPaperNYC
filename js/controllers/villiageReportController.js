43b332f0399c62e9f078013d0a27907f96566905
RyanFairist <ryanfairistrichard@gmail.com>
2016-02-23 12:47:13 -0500

    add sized images to img and fill in the data for the controller side articles

---
+++ js/controllers/villiageReportController.js
@@ -0,0 +1,32 @@
+app.controller('villiageReportController',['$scope', function($scope){
+	$scope.article=[
+		//Dropdown a snip bit of the article  and click for full article..a link..-->
+		{
+		img: "img/doorFrontS.jpg",
+		title: "Kid Builds 87th Street apt </br>with PayDough",
+		author: "Greg Linkseronni"
+       },
+       {
+        img:"img/kidsPlayS",
+        title:"Man in Red or Child in Red",
+        author: "Colin Taylor"
+       },
+       {
+       	img:"img/dinoS.jpg",
+       	title: "Dinos will Take Over </br> Central Park",
+       	author:"Garrett Stevens"
+	   },
+	   {
+	   	img:"img/grandCentralS.jpg",
+	   	title: "Clean Car Trains</br> Do you have a ticket?",
+	   	author: "Allie Levitas"
+	   },
+	   {
+	   	img:"img/oneEuroS.jpg",
+	   	title: "East Village Dollar or Euro",
+	   	author: "Erin Middleton"
+	   }
+
+		]
+	}]);
+

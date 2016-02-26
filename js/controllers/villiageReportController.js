2b3eaa6384e1171af7d3aecac3b92a476a4b437d
RyanFairist <ryanfairistrichard@gmail.com>
2016-02-25 23:01:00 -0500

    add 4 5 6 jpgs. test showme info

--- js/controllers/villiageReportController.js
+++ js/controllers/villiageReportController.js
@@ -25,10 +25,20 @@ app.controller('villiageReportController',['$scope', function($scope){
 	   	img:"img/oneEuroS.jpg",
 	   	title: "East Village Dollar or Euro",
 	   	author: "Erin Middleton"
-	   }
+	   },
+	   four:"Delayed",
+	   five:"For",
+	   six:"Ever"
+	}
 
 		]
 
-		$scope.today=new Date();
+		//$scope.today=new Date();
 	}]);
 
+app.controller('myCtrl', function($scope) {
+    $scope.showMe = false;
+    $scope.myFunc = function() {
+        $scope.showMe = !$scope.showMe;
+    }
+});
